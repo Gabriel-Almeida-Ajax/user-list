eb8cf7937c5c151d2427b8472eb5b9fab9c72058
Gabriel Lima <gabriel.lima@grupoge21.com>
2024-06-22 09:08:36 -0300

    refactor: separa componente do principal page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { UserList } from "./pages/user-list";
+import { UserListPage } from "./pages/user-list";
 
 import "./style/tailwind.css";
 
 function App() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center overflow-y-hidden">
-      <UserList />
+    <div className="flex flex-col h-screen justify-center items-center overflow-x-hidden">
+      <UserListPage />
     </div>
   );
 }
