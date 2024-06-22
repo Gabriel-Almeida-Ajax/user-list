40503e64dff4e73d01b93831d7bb17e6f0013fe1
Gabriel Lima <gabriel.lima@grupoge21.com>
2024-06-22 09:10:12 -0300

    feat: implementa o service de filtro

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -2,8 +2,9 @@ import { User, UserHttpResponse } from "../../@types/user";
 import { httpClient } from "../httpClient";
 import { IUserByIdService } from "./get-user-by-id";
 import { IUserListService } from "./get-user-list";
+import { IUserListFilterService } from "./get-user-list-filtering";
 
-export class UserService implements IUserByIdService, IUserListService {
+export class UserService implements IUserByIdService, IUserListService, IUserListFilterService {
     async getUserById(id: number): Promise<User> {
         const { data } = await httpClient.get<User>(`/users/${id}`);
         return data;
@@ -12,9 +13,14 @@ export class UserService implements IUserByIdService, IUserListService {
         const { data } = await httpClient.get<UserHttpResponse>("/users");
         return data.users;
     }
+    async getUserListFiltering(query?: string | undefined): Promise<User[]> {
+        const { data } = await httpClient.get<UserHttpResponse>(`/users/search?q=${query}`);
+        return data.users;
+    }
 }
 
 export { GetUserList } from "./get-user-list";
+export { GetUserListFiltering } from "./get-user-list-filtering";
 export { GetUserById } from "./get-user-by-id";
 
 export const service = new UserService();
