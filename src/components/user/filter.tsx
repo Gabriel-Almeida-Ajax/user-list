4aba3be6138dccc7a7201940639b81c8b02adf41
Gabriel Lima <gabriel.lima@grupoge21.com>
2024-06-22 09:07:20 -0300

    feat: registra estado para filtrar usuários

--- src/components/user/filter.tsx
+++ src/components/user/filter.tsx
@@ -1,5 +1,23 @@
+import { useFormContext } from "react-hook-form";
 import { Input } from "../ui/input";
+import { FILTERING_GET_USER_LIST } from "../../services/user/get-user-list-filtering";
+import debounce from "debounce";
 
-export function UserListFilter(){
-    return <Input className="w-96" placeholder="Pesquise pelo nome..." />
+export function UserFilter() {
+  const form = useFormContext();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    form.setValue(FILTERING_GET_USER_LIST, e.target.value);
+  };
+
+  const register = form.register(FILTERING_GET_USER_LIST);
+
+  return (
+    <Input
+      className="w-96"
+      placeholder="Pesquise pelo nome..."
+      {...register}
+      onChange={debounce(onChange, 500)}
+    />
+  );
 }
