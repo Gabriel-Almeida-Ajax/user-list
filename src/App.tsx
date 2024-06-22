d45101a8323156cf856d15aec380a7ee25668d56
Gabriel Lima <gabriel.lima@grupoge21.com>
2024-06-21 21:40:38 -0300

    implementado mensagem loading e layout básico

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import { UserList } from "./pages/user-list";
 import "./style/tailwind.css";
 
 function App() {
-  return <UserList />;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <UserList />;
+    </div>
+  );
 }
 
 export default App;
--- src/pages/user-list.tsx
+++ src/pages/user-list.tsx
@@ -2,7 +2,13 @@ import { LoopUserImage, UserImage } from "../components/user/photo";
 import { GetUserList, service } from "../services/user";
 
 export function UserList() {
-  const { data } = GetUserList(service);
+  const { data, isLoading } = GetUserList(service);
 
-  return <LoopUserImage render={UserImage} users={data} />;
+  if (isLoading) return <p>Carregando conteúdo...</p>;
+
+  return (
+    <div className="w-full grid grid-cols-2 md:grid-cols-3 gap-3">
+      <LoopUserImage render={UserImage} users={data} />
+    </div>
+  );
 }
