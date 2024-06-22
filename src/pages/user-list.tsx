46a77031971cdbf19ea65cb10761e3c8389fe861
Gabriel Lima <gabriel.lima@grupoge21.com>
2024-06-22 09:08:10 -0300

    refactor: separa componente do principal page

--- src/pages/user-list.tsx
+++ src/pages/user-list.tsx
@@ -1,28 +1,13 @@
-import { UserCard } from "../components/user/card";
-import { UserListFilter } from "../components/user/filter";
-import { UserLoopRender } from "../components/user/loop-render";
-import { GetUserList, service } from "../services/user";
+import { UserFilter } from "../components/user/filter";
+import { UserList } from "../components/user/list";
 
-export function UserList() {
-  const { data, isLoading } = GetUserList(service);
-
-  if (isLoading) return <p>Carregando conteúdo...</p>;
-
-  const marginTop = "72px";
-
+export function UserListPage() {
   return (
-    <div className="w-full h-full overflow-auto">
+    <div className="w-full h-full">
       <div className="fixed top-0 z-50 flex justify-center py-4 w-full h-fit bg-neutral-50">
-        <UserListFilter />
+        <UserFilter />
       </div>
-      <div
-        className="w-full grid grid-cols-2 md:grid-cols-3 gap-3 p-5"
-        style={{
-          marginTop
-        }}
-      >
-        <UserLoopRender render={UserCard} users={data} />
-      </div>
+      <UserList />
     </div>
   );
 }
