4b2cede0065098709e32d383042c4f86ad5e7632
Gabriel Lima <gabriel.lima@grupoge21.com>
2024-06-22 08:28:18 -0300

    fix: chamadas http desnecessárias

--- src/config/react-query.tsx
+++ src/config/react-query.tsx
@@ -1,7 +1,18 @@
 import { PropsWithChildren } from "react";
 import { QueryClientProvider, QueryClient } from "react-query";
 
-const client = new QueryClient();
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+      refetchIntervalInBackground: false
+    }
+  }
+});
 
 export function QueriesProvider({ children }: PropsWithChildren) {
   return <QueryClientProvider client={client} children={children} />;
