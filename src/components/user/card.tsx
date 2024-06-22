652b717b7738e4df02b11cbbaaa47fba57ffb5bc
Gabriel Lima <gabriel.lima@grupoge21.com>
2024-06-22 09:33:20 -0300

    style: adicionado cargo do usuário, e ajustado hover:scale

--- src/components/user/card.tsx
+++ src/components/user/card.tsx
@@ -1,12 +1,19 @@
+import { Badge } from "antd";
 import { User } from "../../@types/user";
 import { UserImage } from "./photo";
 
 export function UserCard(user: User) {
   return (
-    <div className="flex flex-col justify-center items-center w-42 h-60 border hover:bg-neutral-100 hover:cursor-pointer hover:scale-110 transition-all">
-      <UserImage {...user} />
-      <p>{user.firstName} {user.lastName}</p>
-      <p className="font-bold">@{user.username}</p>
+    <div className="hover:scale-110 transition-all">
+      <Badge.Ribbon text={user.company.title} color={user.eyeColor}>
+        <div className="flex flex-col justify-center items-center w-42 h-60 hover:bg-neutral-100 hover:cursor-pointer">
+          <UserImage {...user} />
+          <p>
+            {user.firstName} {user.lastName}
+          </p>
+          <p className="font-bold">@{user.username}</p>
+        </div>
+      </Badge.Ribbon>
     </div>
   );
 }
